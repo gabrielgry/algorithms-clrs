bb9c391aa3b965c5ac86618790f126b3341223bd
Gabriel Grysuk <gabrielgrysuk@gmail.com>
2023-11-25 14:15:07 -0300

    Rename symbol length to size

--- helpers/array.c
+++ helpers/array.c
@@ -3,12 +3,12 @@
 
 #include "array.h"
 
-void print_int_array(int array[], int length)
+void print_int_array(int array[], int size)
 {
 	printf("[");
 
-	for (int index = 0; index < length; index++) {
-		if (index == length - 1) {
+	for (int index = 0; index < size; index++) {
+		if (index == size - 1) {
 			printf("%d", array[index]);
 		} else {
 			printf("%d, ", array[index]);
@@ -18,14 +18,14 @@ void print_int_array(int array[], int length)
 	printf("]\n");
 }
 
-void fill_array_with_shuffled_int(int array[], int length)
+void fill_array_with_shuffled_int(int array[], int size)
 {
-	for (int index = 0; index < length; index++) {
+	for (int index = 0; index < size; index++) {
 		array[index] = index;
 	}
 
-	for (int index = 0; index < length; index++) {
-		int randomIndex = rand() % length;
+	for (int index = 0; index < size; index++) {
+		int randomIndex = rand() % size;
 		printf("\nrandomIndex: %d\n", randomIndex);
 
 		int tmp = array[index];
--- helpers/array.h
+++ helpers/array.h
@@ -1,7 +1,7 @@
 #ifndef ARRAY_HELPERS_H
 #define ARRAY_HELPERS_H
 
-void print_int_array(int array[], int length);
-void fill_array_with_shuffled_int(int array[], int length);
+void print_int_array(int array[], int size);
+void fill_array_with_shuffled_int(int array[], int size);
 
 #endif
